9c54f300b5d62d34d6c224fea74d99e4a6915b87
Gaea <f40670@gmail.com>
2023-06-21 10:30:31 +0800

    add productCardView

--- E-commerce/Components/ProductCardView.swift
+++ E-commerce/Components/ProductCardView.swift
@@ -8,13 +8,52 @@
 import SwiftUI
 
 struct ProductCardView: View {
+    @EnvironmentObject var cartManager: CartManager
+    var product: Product
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("KSecondary")
+            
+            ZStack(alignment: .bottomTrailing){
+                VStack(alignment: .leading){
+                    Image(product.image)
+                        .resizable()
+                        .frame(width: 175, height:  160)
+                        .cornerRadius(12)
+                    
+                    Text(product.name)
+                        .font(.headline)
+                        .padding(.vertical , 1)
+                    
+                    Text(product.supplier)
+                        .foregroundColor(.gray)
+                        .font(.caption)
+                        .padding(.vertical, 0.5)
+                    
+                    Text("$\(product.price)")
+                        .bold()
+                }
+                
+                
+                Button{
+                    cartManager.addToCart(product: product)
+                }label:{
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color("KPrimary"))
+                        .frame(width: 35, height: 35)
+                        .padding(.trailing)
+                }
+            }
+        }
+        .frame(width: 185, height: 260)
+        .cornerRadius(16)
     }
 }
 
 struct ProductCardView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductCardView()
+        ProductCardView(product: productList[0])
+            .environmentObject(CartManager())
     }
 }
